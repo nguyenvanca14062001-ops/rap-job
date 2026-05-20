b98cc6d5c0d740c0806c7be0d908216475eb15d6
mmopro <nguyenvanca14062001@gmail.com>
2026-05-20 19:27:51 +0700

    Update firebase.ts

--- src/firebase.ts
+++ src/firebase.ts
@@ -2,13 +2,13 @@ import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 import { getAuth } from "firebase/auth";
 
-const firebaseConfig = { 
-  apiKey: "AIzaSyCQz9VulU2k-7O5JZhQxEHD1C5oz6HNO2w", // Dán cái chuỗi Key thật vào đây, giữ nguyên ngoặc kép nhé
-  authDomain: "mmo-pro-6f133.firebaseapp.com", 
-  projectId: "mmo-pro-6f133", 
-  storageBucket: "mmo-pro-6f133.firebasestorage.app", 
-  messagingSenderId: "797198646244", 
-  appId: "1:797198646244:web:3be673461006b0896ea5f1" 
+const firebaseConfig = {
+  apiKey: "AIzaSyCQz9VulU2k-7O5JZhQxEHD1C5oz6HNO2w", // Dán cái Key ...HNO2w của Boss vào giữa 2 dấu ngoặc kép này
+  authDomain: "mmo-pro-6f133.firebaseapp.com",
+  projectId: "mmo-pro-6f133",
+  storageBucket: "mmo-pro-6f133.firebasestorage.app",
+  messagingSenderId: "797198646244",
+  appId: "1:797198646244:web:3be673461006b0896ea5f1"
 };
 
 const app = initializeApp(firebaseConfig);
